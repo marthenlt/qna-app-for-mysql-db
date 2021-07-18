cdb1f537c42f017b6620dd24aad4f7bb369e369f
Marthen Luther <marthenlt@gmail.com>
2021-07-18 10:50:44 +0800

    Update graal-with-jfr.sh
    Add support for JFR on Java 11

--- graal-with-jfr.sh
+++ graal-with-jfr.sh
@@ -4,4 +4,8 @@ echo "Run with GraalVM Enterprise (JFR enabled)"
 
 rm -f graal.jfr
 
+# Java 8
 java -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=duration=2m,delay=30s,name=GraalVMJFR,settings=profile,filename=./graal.jfr -XX:FlightRecorderOptions=loglevel=info -jar target/qna-app-for-mysql-db-0.1.jar
+
+# Java 11 (remove -XX:+UnlockCommercialFeatures and -XX:FlightRecorderOptions=loglevel=info ; add -Xlog:jfr+event+system=info)
+# java -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+FlightRecorder -XX:StartFlightRecording=duration=2m,delay=30s,name=GraalVMJFR,settings=profile,filename=./graal.jfr -Xlog:jfr+event+system=info -jar target/qna-app-for-mysql-db-0.1.jar
